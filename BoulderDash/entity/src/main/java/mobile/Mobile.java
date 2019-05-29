95eda4fb96b825cc95c32386cafee85baa0620cf
ahmedkhaled258 <ahmed.khaled@viacesi.fr>
2019-05-29 13:47:45 +0200

    New

---
+++ BoulderDash/entity/src/main/java/mobile/Mobile.java
@@ -0,0 +1,43 @@
+package mobile;
+
+import java.awt.Point;
+import mobile.Direction;
+
+public abstract class Mobile {
+	
+	protected Point position;
+	protected boolean alive;
+	
+	public Mobile (Point position)
+	{
+		this.position = position;
+		this.alive = true;
+		
+	}
+	
+	protected void setPosition(Point point)
+	{
+		this.position = point;
+	}
+	
+	
+	public void move (Direction direction)
+	{
+		switch (direction)
+		{
+		case Direction.RIGHT:
+			this.setPosition(new Point(this.position.getX()+1,this.position.getY()));
+			
+		case Direction.LEFT:
+			this.setPosition(new Point(this.position.getX()-1,this.position.getY()+1));
+			
+		case Direction.UP:
+			this.setPosition(new Point(this.position.getX(),this.position.getY()+1));
+		
+		case Direction.DOWN:
+			this.setPosition(new Point(this.position.getX(),this.position.getY()-1)));
+			
+			
+		}
+	}
+}
---
+++ BoulderDash/entity/src/main/java/motionless/Motionless.java
@@ -0,0 +1,17 @@
+package motionless;
+
+import java.awt.Point;
+
+public abstract class Motionless {
+		
+	/*The position */
+	protected Point position;
+	
+	public Motionless()
+	{
+		this.position = new Point (0,0);
+	}
+	
+	
+
+}
