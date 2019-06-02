dda122693b5c97af564a1cb4aaf14ae98b8b020d
sfr <saija.fahdrachid@viacesi.fr>
2019-06-02 21:52:29 +0200

    added IModel and Model

--- BoulderDash/contract/src/main/java/contract/IModel.java
+++ BoulderDash/contract/src/main/java/contract/IModel.java
@@ -2,35 +2,9 @@ package contract;
 
 import java.util.Observable;
 
-
-
-/**
- * The Interface IModel.
- *
- * @author Jean-Aymeric Diet
- */
-@SuppressWarnings("deprecation")
 public interface IModel {
 
-	/**
-	 * Gets the hello world.
-	 *
-	 * @return the helloworld entity
-	 */
-	//HelloWorld getHelloWorld();
+	char[][] getMap();
 
-	/**
-	 * Load the message.
-	 *
-	 * @param code
-	 *          the code
-	 */
-	void loadHelloWorld(String code);
-
-	/**
-	 * Gets the observable.
-	 *
-	 * @return the observable
-	 */
 	Observable getObservable();
 }
--- BoulderDash/model/src/main/java/model/Model.java
+++ BoulderDash/model/src/main/java/model/Model.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Model {
+import java.io.IOException;
 
+import java.util.Observable;
+
+import contract.IModel;
+
+public final class Model extends Observable implements IModel {
+
+private char map[][];
+
+protected Launcher table;
+/*
+@throws
+ IOException 
+ */
+public Model(int level) throws IOException {
+	super();
+	table = new Launcher(level);
+	this.setMap(table.getTable());
+	}
+	@Override
+	public char[][] getMap() {
+		return map;
+	}
+	
+	public void setMap(char map[][]) {
+	this.map = map;
+	this.setChanged();
+	this.notifyObservers();
+	}
+
+	@Override
+	public Observable getObservable() {
+		return this;
+	}
 }
