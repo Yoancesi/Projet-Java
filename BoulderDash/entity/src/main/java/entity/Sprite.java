4e2c66cb91d28f6ea25f53662278feb7592a25de
sfr <saija.fahdrachid@viacesi.fr>
2019-06-07 10:42:46 +0200

    view works ig

--- BoulderDash/entity/src/main/java/entity/Sprite.java
+++ BoulderDash/entity/src/main/java/entity/Sprite.java
@@ -65,5 +65,20 @@ public class Sprite extends JPanel{
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
